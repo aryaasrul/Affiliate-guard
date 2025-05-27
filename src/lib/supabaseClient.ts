0cff1fed0caf27f99470c43e9073df96752b8627
Arya Zakaria <aryazakaria@MacBook-Pro-Arya.local>
2025-05-27 18:02:40 +0700

    Update progress

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,8 @@
+// filepath: src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-// Ganti dengan data dari Supabase project Paduka
-const supabaseUrl = 'https://ymqvzbqmtuevnhickftn.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltcXZ6YnFtdHVldm5oaWNrZnRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNDY1NjksImV4cCI6MjA2MzgyMjU2OX0.sgY7EckE5n6ubALVLaqzke3nYzWu0xVDTCBDnE1pjc8';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_ANON_KEY
+);
